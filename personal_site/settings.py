e319cabe632f2f2be32a2a9c55cf6902cde48cbc
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2024-08-08 12:56:47 -0500

    moved secrets key to .env file

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-piycgo%vbs3_%xr-fluz$_q^mk-k!pu+y#a2q_&@+kbcv7(41@'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.getenv("ENVIROMENT") == "development":
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '192.168.1.226']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
