3cc7e2700a7a1d1f56a06205ccc6e9b3115e0cf1
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2024-08-30 23:14:06 -0500

    added http and hsts settings

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -131,3 +131,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# HTTP Settings
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+
+# HSTS settings
+SECURE_HSTS_SECONDS = 31536000 # 1 year
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
