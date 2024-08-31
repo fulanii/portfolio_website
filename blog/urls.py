1436a99ae76d0ab30545b2f3a4b4b4f7a94700cf
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2024-08-30 19:19:10 -0500

    added login page, view and url

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.blog_home, name="blog_home"),
+    path("login/", views.blog_login, name="blog_login")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def blog_home(request):
     return render(request, "blog/home.html")
+
+def blog_login(request):
+    return HttpResponse("Welcome to blog login")
