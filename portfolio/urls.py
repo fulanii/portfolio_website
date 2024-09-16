7485a98bec6daf9ca8c751be2885c68a8fa9a7e7
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2024-09-16 18:04:57 -0500

    added /home url

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("home/", views.index, name="home"),
     path("resume/", views.resume, name="resume"),
     path("links/", views.links, name="links"),
 ]
