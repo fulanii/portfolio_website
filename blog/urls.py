f2d7810c5f91aeca588b508318c8727646d836f1
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2024-10-10 11:34:00 -0500

    removed blog login, logout utl and views

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.blog_home, name="blog_home"),
-    path("login/", views.blog_login, name="login"),
-    path("logout/", views.blog_logout, name="logout"),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,26 +12,6 @@ def blog_home(request):
     return render(request, "blog/home.html", {"blogs": all_blog})
 
 
-def blog_login(request):
-    if request.method == "POST":
-        username  = request.POST.get('username')
-        password  = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            request.session['name'] = username
-            return redirect(reverse('dashboard'))
-        else:
-            return render(request, 'blog/login.html', {'error': 'Invalid username or password.'})
-        
-    return render(request, "blog/login.html")
-
-
-def blog_logout(request):
-    logout(request)
-    return redirect('login')
-
-
 def post_detail(request, slug):
     all_blog = Blog.objects.all()
     blog = get_object_or_404(all_blog, slug=slug)
