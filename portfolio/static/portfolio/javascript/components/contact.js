2faf1c6ad5163a00cc0969448df1a0e4722f71fa
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2025-07-03 02:01:23 -0500

    added captcha

--- portfolio/static/portfolio/javascript/components/contact.js
+++ portfolio/static/portfolio/javascript/components/contact.js
@@ -18,7 +18,7 @@ document.getElementById("cta-options").addEventListener("change", function (even
 allForms.forEach(function (indivudualForm) {
     indivudualForm.addEventListener("submit", function (event) {
         event.preventDefault();
-        fetch(this.action, {
+        fetch("/contactforms/", {
             method: this.method,
             body: new FormData(this)
         })
